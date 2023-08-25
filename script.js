edaa61459b9817aa887296ef878292d4c0246211
Jeron Windson <jeron_windson@hotmail.com>
2023-08-25 15:53:24 -0300

    funções JS Concluidas

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-const List = {
-    theNote: [
-        {
-          id: 1,
-          name: 'academia',
-          content: 'treinar'
-        }
-    ]
-  };
-  
-  function addNote(date) {
-    List.theNote.push({
-      id: List.theNote.length +1,
-      name: date.name,
-      content: date.content
-    });
+const localStorageKey = 'to-do-list-gn'
+
+function validadeNewIfExistNote() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let inputValue = document.getElementById('input-new-note').value
+  let exists = values.find(x => x.name == inputValue)
+  return !exists ? false : true
+}
+
+function newTask() {
+  let input = document.getElementById('input-new-note')
+  input.style.border = ''
+
+  //validation
+  if(!input.value) {
+
+    input.style.border = '1px solid red'
+    alert('digite algo para inserir em sua lista')
+
+  } else if(validadeNewIfExistNote()){
+    alert('Já existe uma nota com essa descrição')
   }
-  
-  function takeNote() {
-  return List.theNote;
+  else {
+    //increment to localStorage
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    values.push({
+      name: input.value
+    })
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+    showValues();
   }
-  
-  function updateNote(id, newContent) {
-  const contentForUp = takeNote().find((theNote) => {
-    return theNote.id === id; });
-  console.log(contentForUp)
-  contentForUp.content = newContent
-  };
-  
-  
-  updateNote(1, 'galho')
-  console.log(takeNote());
-  addNote({id: 2, name: 'padaria', content: 'pao'});
-  
-  takeNote();
-  console.log(takeNote());
-  
-  
-  function deletNote(id) {
-  posNote = takeNote().filter((postAtual) => {
-    return postAtual.id !==id;
-  })
-  console.log(posNote);
+  input.value = ''
+}
+
+function showValues() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let list = document.getElementById('to-do-list')
+  list.innerHTML = ''
+  for(let i = 0; i < values.length; i++) {
+    list.innerHTML += `<li>${values[i]['name']}<button id="btn-ok" onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
   }
-  
-  deletNote(2);
-  takeNote();
+}
+
+function removeItem(data) {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+  let index = values.find(x => x.name == data)
+  values.splice(index, 1)
+  localStorage.setItem(localStorageKey, JSON.stringify(values))
+  showValues()
+}
+
+showValues()
