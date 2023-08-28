46d9cb7875669eb93690900d50f5ac4c1aac747b
Jeron Windson <jeron_windson@hotmail.com>
2023-08-28 20:31:35 -0300

    alteração Hover fundo li e background

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const localStorageKey = 'to-do-list-gn'
 
+
 function validadeNewIfExistNote() {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
   let inputValue = document.getElementById('input-new-note').value
@@ -12,12 +13,12 @@ function newTask() {
   input.style.border = ''
 
   //validation
-  if(!input.value) {
+  if (!input.value) {
 
     input.style.border = '1px solid red'
     alert('digite algo para inserir em sua lista')
 
-  } else if(validadeNewIfExistNote()){
+  } else if (validadeNewIfExistNote()) {
     alert('Já existe uma nota com essa descrição')
   }
   else {
@@ -36,7 +37,7 @@ function showValues() {
   let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
   let list = document.getElementById('to-do-list')
   list.innerHTML = ''
-  for(let i = 0; i < values.length; i++) {
+  for (let i = 0; i < values.length; i++) {
     list.innerHTML += `<li>${values[i]['name']}<button id="btn-ok" onclick='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/></svg></button></li>`
   }
 }
